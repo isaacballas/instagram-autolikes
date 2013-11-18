cf0e10d269c49b3b0feee0e5fce93855f52ff306
Jad  Ismael <jadrismail@gmail.com>
2013-11-19 01:57:39 +0300

    Update cb.php

--- instagram 2.0/cb.php
+++ instagram 2.0/cb.php
@@ -2,20 +2,8 @@
 
   require_once 'includes/instagram.class.php';
 
-$instagram = new Instagram(array(
-      'apiKey'      => '63dc1852151d4431b95a706b91581fa6',
-      'apiSecret'   => '3ff6483836324249850217ca5b479eae',
-      'apiCallback' => 'http://www.ultgate.com/instagram2/cb.php'
-    ));
+  require_once 'includes/instaconfig.php';
 
-/*
-$instagram = new Instagram(array(
-      'apiKey'      => '63dc1852151d4431b95a706b91581fa6',
-      'apiSecret'   => '3ff6483836324249850217ca5b479eae',
-      'apiCallback' => 'http://www.ultgate.com/insta/master/cb.php'
-    ));
-
-*/
 if( !$_GET['code'] && !$_COOKIE['ulttoken']){
  echo "<a href='{$instagram->getLoginUrl(array(
   'basic',
@@ -51,31 +39,7 @@ else {
         $instagram->setAccessToken($_COOKIE['ulttoken']);
     echo '<META HTTP-EQUIV="Refresh" Content="0; URL=http://www.ultgate.com/instagram2/">';    
     exit;   
-if (isset($_GET) && count($_GET) > 0 && $_GET['method'])
-{
 
-
-    $method = $_GET['method'];
-
-	switch($method)
-	{
-
-
-
-case 'liketag':
-$instagram->likeTag("hot");
-break;
-
-
-	}
-
-
-
-}
-else
-{
-	exit();
-}
 
 }
 
